11379e833a0d5fdb6892b3a091e8ce54881569e3
mykhailonl <mykhailonl@users.noreply.github.com>
2024-06-18 14:25:21 +0000

    deploy: 1ce3dfaef254b96f605bceadbdd43ac56508efca

--- src/types/Slide.ts
+++
@@ -1,5 +0,0 @@
-export type Slide = {
-  id: number;
-  image: string;
-  link: string;
-};
--- src/types/SliderState.ts
+++
@@ -1,9 +0,0 @@
-import { Slide } from './Slide';
-
-export type SliderState = {
-  currentIndex: number;
-  slides: Slide[];
-  interval: number;
-  autoPlay: boolean;
-  isAnimating: boolean;
-};
