f12683349cc945811b8769cc9b9b3c0fd60e1e52
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-07-25 18:32:07 +0200

    created universal button components

---
+++ src/components/ButtonPrimary/ButtonPrimary.tsx
@@ -0,0 +1,28 @@
+import { CSSProperties } from 'react';
+import styles from './ButtonPrimary.module.scss';
+const { button, button__text } = styles;
+
+type Props = {
+  buttonText: string;
+  action: () => void;
+  disabled: boolean;
+  additionalStyles?: CSSProperties;
+};
+
+export const ButtonPrimary = ({
+  buttonText,
+  action,
+  disabled,
+  additionalStyles,
+}: Props) => {
+  return (
+    <button
+      className={button}
+      onClick={action}
+      style={additionalStyles}
+      disabled={disabled}
+    >
+      <span className={button__text}>{buttonText}</span>
+    </button>
+  );
+};
